ec893cc83ff4e6af89709493077147d3b9c73e56
Andre Meyer <info@andre-meyer.ch>
2016-05-11 14:23:07 +0200

    + updated TimeSpentVisualizerTest again

--- src/client/AM.PA.MonitoringTool/TimeSpentVisualizerTests/Properties/AssemblyInfo.cs
+++ src/client/AM.PA.MonitoringTool/TimeSpentVisualizerTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeSpentVisualizerTest")]
+[assembly: AssemblyTitle("TimeSpentVisualizerTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TimeSpentVisualizerTest")]
+[assembly: AssemblyProduct("TimeSpentVisualizerTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
