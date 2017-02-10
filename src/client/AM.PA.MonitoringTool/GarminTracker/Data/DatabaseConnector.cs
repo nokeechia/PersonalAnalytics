17b10ae18bbf0fd97e28ff435015753c089c4849
s-mueller <smueller@ifi.uzh.ch>
2017-02-10 17:53:25 +0100

    Added Garmin Tracker

---
+++ src/client/AM.PA.MonitoringTool/GarminTracker/Data/DatabaseConnector.cs
@@ -0,0 +1,18 @@
+﻿// Created by Sebastian Mueller (smueller@ifi.uzh.ch) from the University of Zurich
+// Created: 2017-02-10
+// 
+// Licensed under the MIT License.
+
+namespace GarminTracker.Data
+{
+    public class DatabaseConnector
+    {
+
+        internal static void CreateGarminTables()
+        {
+
+        }
+
+    }
+
+}
---
+++ src/client/AM.PA.MonitoringTool/GarminTracker/Settings.cs
@@ -0,0 +1,18 @@
+﻿// Created by Sebastian Mueller (smueller@ifi.uzh.ch) from the University of Zurich
+// Created: 2017-02-10
+// 
+// Licensed under the MIT License.
+
+
+namespace GarminTracker
+{
+    internal class Settings
+    {
+
+        internal const string TRACKER_NAME = "GarminTracker";
+        internal const bool IS_DETAILED_COLLECTION_AVAILABLE = true;
+        internal const string TRACKER_ENABLED = "GarminTrackerEnabled";
+
+    }
+
+}
